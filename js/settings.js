53373a3b92e3cadd5c363791a70fa6f36958f0cb
Paul Thrasher <thrashr888@gmail.com>
2013-01-07 21:54:22 -0800

    Update js/settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,6 +1,6 @@
 window.s3u = {};
-window.s3u.bucket = "thrashr888-tests3upper";
-window.s3u.AWSAccessKeyId = "AKIAIQBDDUC7LR5CMLOA";
-window.s3u.policy = "eyJleHBpcmF0aW9uIjogIjIwMjAtMDEtMDFUMDA6MDA6MDBaIiwgICJjb25kaXRpb25zIjogWyAgICBbImVxIiwgIiRidWNrZXQiLCAidGhyYXNocjg4OC10ZXN0czN1cHBlciJdLCAgICBbInN0YXJ0cy13aXRoIiwgIiRrZXkiLCAidS8iXSwgICAgeyJhY2wiOiAicHVibGljLXJlYWQifSwgICAgWyJzdGFydHMtd2l0aCIsICIkQ29udGVudC1UeXBlIiwgIiJdLCAgICBbImNvbnRlbnQtbGVuZ3RoLXJhbmdlIiwgMiwgNTI0Mjg4MF0gIF19";
-window.s3u.signature = "DB/MNwYMikm1ZNWzMlRZqyCF/B0=";
+window.s3u.bucket = "";
+window.s3u.AWSAccessKeyId = "";
+window.s3u.policy = "";
+window.s3u.signature = "";
 window.s3u.key = "u/";
