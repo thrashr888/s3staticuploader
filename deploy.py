7161233b0afdcc510759c293ce07b2be0702920d
Paul Thrasher <thrashr888@gmail.com>
2021-09-14 09:47:47 -0700

    update to python3

--- deploy.py
+++ deploy.py
@@ -19,7 +19,7 @@ from boto.s3.lifecycle import Lifecycle
 from boto.s3.lifecycle import Rule
 from boto.exception import S3ResponseError
 
-print "Usage: deploy.py BUCKET_NAME AWS_ACCESS_KEY AWS_SECRET_KEY\n"
+print("Usage: deploy.py BUCKET_NAME AWS_ACCESS_KEY AWS_SECRET_KEY\n")
 
 if sys.argv.__len__() < 4:
     quit()
@@ -35,28 +35,30 @@ conn = S3Connection(aws_access_key, aws_secret_key)
 # only set up if bucket doesn't already exist
 try:
     bucket = conn.get_bucket(bucket_name, True)
-    print "Using bucket `%s`" % (bucket_name)
+    print("Using bucket `%s`" % (bucket_name))
 
 except S3ResponseError:
     bucket = False
-    print "Bucket doesn't exist `%s`" % (bucket_name)
+    print("Bucket doesn't exist `%s`" % (bucket_name))
 
 if not bucket:
 
-    print "Creating new bucket `%s`..." % (bucket_name)
+    print("Creating new bucket `%s`..." % (bucket_name))
 
     bucket = conn.create_bucket(bucket_name)
     bucket.configure_website('index.html')
 
-    print "Adding CORS settings..."
+    print("Adding CORS settings...")
 
     cors_cfg = CORSConfiguration()
-    cors_cfg.add_rule(['PUT', 'POST', 'DELETE'], 'http://' + bucket_name + '.' + region, allowed_header='*', max_age_seconds=3000, expose_header='x-amz-server-side-encryption')
-    cors_cfg.add_rule(['PUT', 'POST', 'DELETE'], 'http://localhost', allowed_header='*', max_age_seconds=3000, expose_header='x-amz-server-side-encryption')
+    cors_cfg.add_rule(['PUT', 'POST', 'DELETE'], 'http://' + bucket_name + '.' + region,
+                      allowed_header='*', max_age_seconds=3000, expose_header='x-amz-server-side-encryption')
+    cors_cfg.add_rule(['PUT', 'POST', 'DELETE'], 'http://localhost', allowed_header='*',
+                      max_age_seconds=3000, expose_header='x-amz-server-side-encryption')
     cors_cfg.add_rule('GET', '*')
     bucket.set_cors(cors_cfg)
 
-    print "Adding Lifecycle settings..."
+    print("Adding Lifecycle settings...")
 
     lifecycle_cfg = Lifecycle()
     lifecycle_cfg.add_rule('d1', 'u/d1/', 'Enabled', 1)
@@ -70,21 +72,21 @@ if not bucket:
     lifecycle_cfg.add_rule('d30', 'u/d30/', 'Enabled', 30)
     bucket.configure_lifecycle(lifecycle_cfg)
 
-print "Uploading site files..."
+print("Uploading site files...")
 
 # Only upload the files we need
 files = [
-['./', 'index.html'],
-['./css/', 'bootstrap.min.css'],
-['./css/', 'bootstrap-responsive.min.css'],
-['./css/', 'main.css'],
-['./js/', 'main.js'],
-['./js/', 'moment.min.js'],
-['./js/', 'settings.js'],
-['./js/vendor/', 'bootstrap.min.js'],
-['./js/vendor/', 'jquery-1.8.2.min.js'],
-['./js/vendor/', 'modernizr-2.6.1.min.js'],
-['./flash/', 'clippy.swf'],
+    ['./', 'index.html'],
+    ['./css/', 'bootstrap.min.css'],
+    ['./css/', 'bootstrap-responsive.min.css'],
+    ['./css/', 'main.css'],
+    ['./js/', 'main.js'],
+    ['./js/', 'moment.min.js'],
+    ['./js/', 'settings.js'],
+    ['./js/vendor/', 'bootstrap.min.js'],
+    ['./js/vendor/', 'jquery-1.8.2.min.js'],
+    ['./js/vendor/', 'modernizr-2.6.1.min.js'],
+    ['./flash/', 'clippy.swf'],
 ]
 
 matches = []
@@ -93,7 +95,8 @@ for root, filename in files:
     k = Key(bucket)
     k.key = file.replace("./", "")
     k.set_contents_from_filename(file, policy=CannedACLStrings[1])
-    print k.key
+    print(k.key)
 
 website_endpoint = bucket.get_website_endpoint()
-print "\nDone! Website deployed to:\n\n\033[1;32mhttp://%s/\033[0m\n" % (website_endpoint)
+print(
+    "\nDone! Website deployed to:\n\n\033[1;32mhttp://%s/\033[0m\n" % (website_endpoint))
--- policy.py
+++ policy.py
@@ -10,10 +10,11 @@
 
 import base64
 import hmac
-import sha
 import sys
+import hashlib
 
-print "Usage: policy.py BUCKET_NAME AWS_ACCESS_KEY AWS_SECRET_KEY [KEY] [MAX_MEGABYTES] [ACL]\n"
+print(
+    "Usage: policy.py BUCKET_NAME AWS_ACCESS_KEY AWS_SECRET_KEY [KEY] [MAX_MEGABYTES] [ACL]\n")
 
 if sys.argv.__len__() < 4:
     quit()
@@ -22,7 +23,9 @@ bucket_name = sys.argv[1]
 aws_access_key = sys.argv[2]
 aws_secret_key = sys.argv[3]
 key = sys.argv[4] if sys.argv.__len__() >= 5 else "u/"
-content_length_range = int(float(sys.argv[5]) * 1048576) if sys.argv.__len__() >= 6 else 5242880  # 5mb default. convert to bytes.
+# 5mb default. convert to bytes.
+content_length_range = int(
+    float(sys.argv[5]) * 1048576) if sys.argv.__len__() >= 6 else 5242880
 acl = sys.argv[6] if sys.argv.__len__() >= 7 else "public-read"
 
 # build the policy string
@@ -36,10 +39,13 @@ policy_document = """{"expiration": "2020-01-01T00:00:00Z",
   ]
 }""" % (bucket_name, key, acl, content_length_range)
 
-policy = base64.b64encode(policy_document.replace('\n', ''))
+policy = base64.b64encode(bytes(policy_document.replace('\n', ''), 'utf-8'))
 
 signature = base64.b64encode(
-    hmac.new(aws_secret_key, policy, sha).digest())
+    hmac.new(bytes(aws_secret_key, 'utf-8'),
+             policy,
+             hashlib.sha256).digest()
+)
 
 # build JS settings to write to file
 settings = """window.s3u = {};
@@ -50,13 +56,13 @@ window.s3u.signature = "%s";
 window.s3u.key = "%s";""" % (bucket_name, aws_access_key, policy, signature, key)
 
 # print everything out
-print "policy_document:\n" + policy_document + "\n"
-print "policy:\n" + policy + "\n"
-print "signature:\n" + signature + "\n"
-print "settings:\n" + settings + "\n"
+print("policy_document:\n" + policy_document + "\n")
+print("policy:\n" + str(policy) + "\n")
+print("signature:\n" + str(signature) + "\n")
+print("settings:\n" + settings + "\n")
 
 # update the file
 f = open('js/settings.js', 'w')
 f.write(settings)
 
-print "Saved to js/settings.js.\n"
+print("Saved to js/settings.js.\n")
